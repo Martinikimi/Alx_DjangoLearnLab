65a57434232e464ca214db985629e75f5ae596e0
Martin Kimani <martinikimi7@gmail.com>
2025-09-10 19:21:54 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -33,11 +33,13 @@ urlpatterns = [
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
 ]
+]
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Book permissions
+    # Book permission views
     path('book/add/', views.add_book, name='add_book'),
     path('book/edit/<int:pk>/', views.edit_book, name='edit_book'),
     path('book/delete/<int:pk>/', views.delete_book, name='delete_book'),
@@ -46,5 +48,4 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
-
 
