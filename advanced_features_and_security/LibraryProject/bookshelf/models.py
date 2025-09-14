e894746701abd788bd761973abfe4ae210dbf3e1
Martin Kimani <martinikimi7@gmail.com>
2025-09-14 22:00:09 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -26,3 +26,29 @@ class CustomUser(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    # Extend if needed
+    pass
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can View Book"),
+            ("can_create", "Can Create Book"),
+            ("can_edit", "Can Edit Book"),
+            ("can_delete", "Can Delete Book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
+
