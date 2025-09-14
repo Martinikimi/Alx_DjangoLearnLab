6a25529d41d56b03b0e631ddcd685b736a140e6e
Martin Kimani <martinikimi7@gmail.com>
2025-09-14 22:20:07 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm 
 
 
+def example_view(request):
+    """
+    A simple view to demonstrate usage of ExampleForm.
+    """
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process form data here
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            # For now, just redirect to a success page
+            return redirect('success')
+    else:
+        form = ExampleForm()
+    
+    return render(request, 'bookshelf/example_form.html', {'form': form})
+    
 @permission_required("bookshelf.can_view", raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
