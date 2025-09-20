002d9963cb53ae4c2f9a30fa40cb196241cfdac2
Martin Kimani <martinikimi7@gmail.com>
2025-09-21 00:42:48 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,1 +1,8 @@
 
+from django.shortcuts import render
+from .models import Book
+
+# Function-based view
+def list_books(request):
+    books = Book.objects.all()   # ✅ required
+    return render(request, "relationship_app/list_books.html", {"books": books})  # ✅ required
