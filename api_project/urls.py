6b88eddd3906200efedccc10ba805511059dd3b6
Martin Kimani <martinikimi7@gmail.com>
2025-09-21 00:20:21 +0300

    Update urls.py

--- api_project/urls.py
+++ api_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include   # 👈 make sure include is here!
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),   # 👈 this is what the checker wants
 ]
+
