e3c60bf98a4b45bd3b8227e23b328618ac1ffd50
Martin Kimani <martinikimi7@gmail.com>
2025-09-21 00:58:34 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -18,3 +18,20 @@ class LibraryDetailView(DetailView):
     model = Library                # ✅ tells Django which model to use
     template_name = "relationship_app/library_detail.html"  # ✅ required
     context_object_name = "library"  # ✅ checker expects "library"
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+# Registration view
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log the user in immediately
+            return redirect("list_books")  # redirect to your book list or any home page
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
+
