4f558b3591e12f428ab5515651a55a128c53400e
Martin Kimani <martinikimi7@gmail.com>
2025-09-21 00:37:58 +0300

    Create views.py

---
+++ django-models/relationship_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Library
+from django.views.generic import DetailView
+
+# Function-based view
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
