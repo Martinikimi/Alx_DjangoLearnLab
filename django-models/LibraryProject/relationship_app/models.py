65af7504622194ef021ab682d1f5b17b4be43339
Martin Kimani <martinikimi7@gmail.com>
2025-09-21 01:02:50 +0300

    Create models.py

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ("Admin", "Admin"),
+        ("Member", "Member"),
+        ("Librarian", "Librarian"),  # Added Librarian role
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES, default="Member")
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
