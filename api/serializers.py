6ad9a2fdc98d8b3cce57459884c941f14027ad0d
Martin Kimani <martinikimi7@gmail.com>
2025-10-04 17:36:01 +0300

    Initial commit for Django blog

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Book
-
-#Book serializer
-class BookSerializer(serializers.ModelSerializer):
-    class meta:
-        model = Book
-        fields='__all__'
