5155a817a98f267e6193abd6b5e78f0505e3cc4d
Martin Kimani <martinikimi7@gmail.com>
2025-10-04 18:41:53 +0300

    Database structure

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
